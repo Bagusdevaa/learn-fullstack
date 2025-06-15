f53010aaba17d6b0f14aee60e5e668c73f8f3cbb
Bagusdevaa <deva999official@gmail.com>
2025-06-16 04:01:22 +0800

    fix: remove the parameter in Login, Register, and Dashboard

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home title="INI HALAMAN HOME"/>}/>
-        <Route path="/login" element={<Login title="LOGIN PAGE" description="MINI ABSENSI APPS"/>}/>
-        <Route path="/register" element={<Register title="REGISTER PAGE" description="MINI ABSENSI APPS"/>}/>
-        <Route path="/dashboard" element={<Dashboard title="DASHBOARD" />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
