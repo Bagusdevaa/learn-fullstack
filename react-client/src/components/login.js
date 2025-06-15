ddd65aff5e769937e833a9fb67d52a394d93c7ce
Bagusdevaa <deva999official@gmail.com>
2025-06-16 04:01:38 +0800

    fix: Updated UI for login page

--- react-client/src/components/login.js
+++ react-client/src/components/login.js
@@ -1,9 +1,9 @@
-import Typewriter from 'typewriter-effect';
 import { Container, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 import { useState } from "react"
 import axios from "axios";
 
-const Login = ( {title, description} ) => {
+const Login = () => {
     const [NIP, setNIP] = useState("")
     const [password, setPassword] = useState("")
 
@@ -22,7 +22,7 @@ const Login = ( {title, description} ) => {
             method: 'POST',
             url: 'http://localhost:3200/users/login',
             data: requestingData
-        }).then((result)=> {
+        }).then((result) => {
             localStorage.setItem('nip', result.data.users.nip)
             localStorage.setItem('nama', result.data.users.nama)
             window.location.replace('/dashboard')
@@ -30,30 +30,34 @@ const Login = ( {title, description} ) => {
     }
 
     return (
-        <Container>
-            <div className="d-flex justify-content-center fw-bold h3 my-4">
-                <Typewriter
-                    options={{
-                        strings: [title, description],
-                        autoStart: true,
-                        loop: true,
-                        delay: 100,
-                        deleteSpeed: 50,
-                    }}
-                />
-            </div>
-            <form className="w-50 mx-auto">
-                <Form.Group>
-                    <Form.Label className='fw-bold'>NIP</Form.Label>
-                    <Form.Control type='number' placeholder='NIP Anda' required onChange={(event) => handleNIP(event.target.value)} />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className='fw-bold'>Password</Form.Label>
-                    <Form.Control type='password' placeholder='Password Anda' required onChange={(event) => handlePassword(event.target.value)} />
-                </Form.Group>
-                <Button className="mt-4 w-100" onClick={() => userLogin()}>Login Sekarang</Button>
-            </form>
-        </Container>
+        <div className='login-page'>
+            <Container>
+                <div className='vh-100 d-flex flex-column justify-content-center'>
+                    <div className='login-border col-7 m-auto p-5 bg-light rounded'>
+                        <Link to='/' className='text-decoration-none'>
+                            <h1 className="fw-bold h3 text-center">
+                                MASUK AKUN
+                            </h1>
+                        </Link>
+                        <form className="m-auto">
+                            <Form.Group>
+                                <Form.Label className='form-title fw-bold'>NIP</Form.Label>
+                                <Form.Control type='number' placeholder='NIP Anda' required onChange={(event) => handleNIP(event.target.value)} />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label className='form-title fw-bold'>Password</Form.Label>
+                                <Form.Control type='password' placeholder='Password Anda' required onChange={(event) => handlePassword(event.target.value)} />
+                            </Form.Group>
+                            <Button className="mt-4 w-100 btn-light" onClick={() => userLogin()}>Login Sekarang</Button>
+                            <p className='pt-4 text-center'>
+                                Belum punya akun?
+                                <Link to='/register' className='register-direct text-decoration-none'> Daftar di sini</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </Container>
+        </div>
     )
 }
 
