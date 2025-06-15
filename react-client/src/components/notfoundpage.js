dd533bd49671cd926759aef2e0c9720ca120d453
Bagusdevaa <deva999official@gmail.com>
2025-06-15 20:53:42 +0800

    fix: Fixed 404 not found page

--- react-client/src/components/notfoundpage.js
+++ react-client/src/components/notfoundpage.js
@@ -8,13 +8,15 @@ const NotFound = () => {
         <div className='pageNotFound'>
             <Container>
                 <div className='vh-100 d-flex align-items-center justify-content-center flex-column text-white'>
-                    <div className=''>
+                    <div>
                         <h1 className='me-auto'>404😔</h1>
-                        <h2>Oops. Halaman ini tidak ada.</h2>
+                        <h2 className='mt-3'>Oops. Halaman ini tidak ada.</h2>
                         <p className='my-3'>Kamu mungkin salah ketik alamat web atau halaman sudah dipindahkan.</p>
-                        <Link to='/'>
-                            <Button className='btn-outline-light mt-2'>Halaman Utama</Button>
-                        </Link>
+                        <div className='d-flex flex-column d-lg-block'>
+                            <Link to='/'>
+                                <Button size='lg' className='btn-outline-light mt-2 col-12 col-lg-5'>Halaman Utama</Button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </Container>
